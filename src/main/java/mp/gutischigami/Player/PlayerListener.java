6b6807a34edd32d2a5abcced16be2fbe7bb543ce
Gütischigami <gustavo.solar.jeraldo@gmail.com>
2024-06-03 23:20:43 -0400

    Creacion de metodo onMode

--- src/main/java/mp/gutischigami/Player/PlayerListener.java
+++ src/main/java/mp/gutischigami/Player/PlayerListener.java
@@ -1,8 +1,11 @@
 package mp.gutischigami.Player;
-
 import mp.gutischigami.PluginGMKN;
+import mp.gutischigami.Utils.MessageUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
-
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import java.net.http.WebSocket;
 
 public class PlayerListener implements WebSocket.Listener {
@@ -13,4 +16,11 @@ public class PlayerListener implements WebSocket.Listener {
         this.plugin = plugin;
     }
 
+    @EventHandler
+    public void onMode(PlayerGameModeChangeEvent event){
+    Player player = event.getPlayer();
+        if (player.getGameMode().equals(GameMode.CREATIVE)){
+            Bukkit.getServer().broadcastMessage(MessageUtils.getColoredMessage("&cQue pasó &5 " + player.getName() + " &cme salió &d WEKEREKE?"));
+        }
+    }
 }
